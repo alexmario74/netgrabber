c740a8164ec41642d7cf37f99b4d9c7bbff82877
Mario A. Santini <alexmario74@gmail.com>
2017-10-21 11:44:10 +0200

    refine assertions

--- test/device-test.js
+++ test/device-test.js
@@ -11,17 +11,17 @@ describe('Netgrabber should handle devices',
 
     it('If no id is provided, the device should return null', () => {
         const device = MakeDevice();
-        assert(device.id() == null);
+        assert.equal(device.id(), null);
     })
 
     it('A device should have a name', () => {
         const device = MakeDevice({name: 'device1'});
-        assert(device.name() == 'device1');
+        assert.equal(device.name(), 'device1');
     })
 
     it('If no name is provided, the device should return null', () => {
         const device = MakeDevice();
-        assert(device.name() == null);
+        assert.equal(device.name(), null);
     })
 
     it("A device should have a list of measures", () => {
@@ -33,9 +33,9 @@ describe('Netgrabber should handle devices',
     () => {
         const device = MakeDevice();
         const measures = device.availableMeasures();
-        assert(typeof measures === 'object');
+        assert.strictEqual(typeof measures, 'object');
         assert(Array.isArray(measures));
-        assert(measures.length == 0);
+        assert.strictEqual(measures.length, 0);
     })
 
     it('Should be possible to have multiple device each different',
@@ -44,25 +44,25 @@ describe('Netgrabber should handle devices',
         const device2 = MakeDevice({id:2,name:'device2',measures: [""]});
         const device3 = MakeDevice({id:3,name:'device3',measures: ["", ""]});
 
-        assert(device1.id() == 1)
-        assert(device2.id() == 2) 
-        assert(device3.id() == 3)
+        assert.equal(device1.id(), 1);
+        assert.equal(device2.id(), 2);
+        assert.equal(device3.id(), 3);
 
-        assert(device1.name() == 'device1')
-        assert(device2.name() == 'device2') 
-        assert(device3.name() == 'device3')
+        assert.equal(device1.name(), 'device1');
+        assert.equal(device2.name(), 'device2');
+        assert.equal(device3.name(), 'device3');
         
-        assert(device1.availableMeasures().length == 0)
-        assert(device2.availableMeasures().length == 1) 
-        assert(device3.availableMeasures().length == 2)
+        assert.equal(device1.availableMeasures().length, 0);
+        assert.equal(device2.availableMeasures().length, 1); 
+        assert.equal(device3.availableMeasures().length, 2);
     })
 
     it('Given a device it should be possible to serialize', () => {
         const device = MakeDevice({id: 1, name:'mydevice', measures: [""]});
 
-        assert(device.serialize() == '{"id":1,"name":"mydevice","measures":[""]}',
-        'serialized device should be {"id":1,"name":"mydevice","measures":[""]}\nGot\n' + 
-            device.serialize());
+        assert.equal(device.serialize(),
+         '{"id":1,"name":"mydevice","measures":[""]}',
+        'serialized device should be {"id":1,"name":"mydevice","measures":[""]}');
     })
 
     it('It should be possible to make a device from a serialized string', 
@@ -76,6 +76,6 @@ describe('Netgrabber should handle devices',
             (resumedDevice.availableMeasures().length === device.availableMeasures().length));
         
         const emptyDevice = MakeDevice.FromSerialized();
-        assert(emptyDevice.id() === null);
+        assert.strictEqual(emptyDevice.id(), null);
     })
 });
