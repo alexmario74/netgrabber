06d9ad19e26794f7b79879f944385164e077f09a
Mario A. Santini <alexmario74@gmail.com>
2017-10-28 09:00:12 +0200

    added main app script

---
+++ src/main.js
@@ -0,0 +1,45 @@
+const lokijs = require('lokijs');
+const debug = require('debug')('netgrabber');
+
+const MakeDevice = require('./netgrabber/device');
+const MakeMeasure = require('./netgrabber/measure');
+const { MakeCache } = require('./service/lokidb');
+const { MakeDeviceSource } = require('./service/device-source');
+
+const { loadDeviceFromService, storeDeviceInCache } = require('./netgrabber/tasks');
+
+debug('load dependencies');
+
+const main = (db) => {
+    ['SIGINT', 'SIGILL'].forEach((signal) => 
+        process.on(signal, () => {
+            db.close();
+            process.exit();
+        }));
+
+    debug('db loaded');
+    
+    const deviceCache = MakeCache(db, 'device');
+    const measureCache = MakeCache(db, 'measure');
+    const deviceSource = MakeDeviceSource();
+
+    setInterval(() => {
+        deviceSource.loadNewDevices()
+            .then(devices => 
+                storeDeviceInCache(deviceCache, 
+                    devices.map(MakeDevice)))
+            .catch(errorHandling);
+        }, 20000);
+}
+
+const errorHandling = (err) => {
+    debug('ERROR', err);
+}
+
+let db = new lokijs('netgrabber.json',
+{
+    autoload: true,
+    autoloadCallback : () => (main(db)),
+    autosave: true, 
+    autosaveInterval: 4000
+});
